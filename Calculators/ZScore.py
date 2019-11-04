1bcbb310ffbe19613486372761c185c460a44695
ncl7 <ncl7@njit.edu>
2019-11-04 18:09:27 -0500

    Feature: added code to calculate Z Score

---
+++ Calculators/ZScore.py
@@ -0,0 +1,9 @@
+from Calculators.PopulationMean import population_mean
+
+
+def zscore(a, b, c):
+    a = # population #
+    b = population_mean
+    c = # standard deviation
+    d = (a - b) / c
+    return d
