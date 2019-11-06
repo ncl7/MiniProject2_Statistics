176e944e62fd0c2a2d38b0c48cd262b4e21c29ae
ncl7 <ncl7@njit.edu>
2019-11-06 17:46:28 -0500

    Feature: created Statistics class extending Calculator and added zscore

--- Statistics/Statistics.py
+++ Statistics/Statistics.py
@@ -1,1 +1,11 @@
+from Calculators.Calculator import Calculator
 
+
+class Statistics(Calculator):
+    data = []
+
+    def __init__(self):
+
+    def zscore(self, a, b, c):
+        self.result = zscore(a, b, c)
+        return self.result
