8a33be0d72066de9d6b4bb22295d0c1f12c40e4c
ncl7 <ncl7@njit.edu>
2019-11-06 21:27:07 -0500

    Feature: created code/pseudocode for population variance

--- Calculators/PopulationVar.py
+++ Calculators/PopulationVar.py
@@ -1,3 +1,10 @@
 # Population Variance
+from Calculators.PopulationMean import population_mean
+
 
 def population_variance(a, b, c):
+    a = float(population_mean)
+    # b = list(population data-points minus mean)
+    c = list(b^2)
+    d = (addition(c))/5000
+    return d
