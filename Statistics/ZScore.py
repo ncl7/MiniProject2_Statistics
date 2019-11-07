0b82483d7cceda7fc8f72feec94b73baeb0c3b1e
Kelley Shann <kelleyshann@gmail.com>
2019-11-07 17:07:50 -0500

    task: updated z-score formula

--- Statistics/ZScore.py
+++ Statistics/ZScore.py
@@ -1,8 +1,9 @@
 from Statistics.PopulationMean import population_mean
 from Statistics.SampleStandardDeviation import sampleSTD
+from Calculators.Division import division
 
 
 def z_score(data):
     u = population_mean(data)
     sam_std = sampleSTD(data)
-    return (len(data) - u) / sam_std
+    return division((len(data) - u), sam_std)
