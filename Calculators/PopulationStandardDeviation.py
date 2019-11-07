dd3bfb1c4c56a7bcf113dafa22c42ce8fed3fbf4
Kelley Shann <kelleyshann@gmail.com>
2019-11-06 22:12:08 -0500

    feature: added standard dev function

---
+++ Calculators/PopulationStandardDeviation.py
@@ -0,0 +1,13 @@
+from Calculators.PopulationMean import population_mean
+from Calculators.SquareRoot import square_root
+
+
+def pop_stand_dev(data):
+    n = len(data)
+    u = population_mean(data)
+    return square_root(sum([(element - u) ** 2 for element in data]) / (len(data) - 1))
+
+
+
+
+
