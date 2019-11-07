71cdabdf738631a2e64bfbaf445d2f1866166539
Kelley Shann <kelleyshann@gmail.com>
2019-11-07 18:28:10 -0500

    task: added test for pop mean

---
+++ Tests/test_Statistics.py
@@ -0,0 +1,23 @@
+import unittest
+from CSVReader.CSVReader import CsvReader
+
+from Statistics.Statistics import Statistics
+import pprint
+
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.statistics = Statistics()
+
+    def test_instantiate_calculator(self):
+        self.assertIsInstance(self.statistics, Statistics)
+
+    def test_pop_mean(self):
+        test_data = CsvReader('Tests/Data/female_height.csv').data
+        for row in test_data:
+            self.assertEqual(self.statistics.pop_mean())
+
+
+
+if __name__ == '__main__':
+    unittest.main()
