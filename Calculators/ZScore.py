7453b8a694916234e95211ab3d122480dddaec94
ncl7 <ncl7@njit.edu>
2019-11-07 19:14:08 -0500

    Fix: deleted old ZScore file in Calculators directory

--- Calculators/ZScore.py
+++
@@ -1,7 +0,0 @@
-from Calculators.PopulationMean import population_mean
-from Calculators.sampleSTD import sampleSTD
-
-
-def z_score():
-    d = (len(list) - population_mean) / sampleSTD
-    return d
