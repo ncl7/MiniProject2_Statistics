b1833531504120d74551cca9e089d42bafbaf916
ncl7 <ncl7@njit.edu>
2019-11-07 19:02:38 -0500

    Feature: created more variables for calculating z-score, using methods from Calculators

--- Statistics/ZScore.py
+++ Statistics/ZScore.py
@@ -1,9 +1,13 @@
 from Statistics.PopulationMean import population_mean
 from Statistics.SampleStandardDeviation import sampleSTD
 from Calculators.Division import division
+from Calculators.Subtraction import subtraction
 
 
 def z_score(data):
     u = population_mean(data)
-    sam_std = sampleSTD(data)
-    return division((len(data) - u), sam_std)
+    x = len(data)
+    y = subtraction(x, u)
+    sample_sd = sampleSTD(data)
+    z = division(sample_sd, y)
+    return z
