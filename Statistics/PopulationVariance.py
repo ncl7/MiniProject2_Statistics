3a6be9b013c94e8eac1e56123d5e9337efdce938
ncl7 <ncl7@njit.edu>
2019-11-07 19:40:16 -0500

    Feature: added more variables to calculate Population Variance, utilizing methods in Calculators

--- Statistics/PopulationVariance.py
+++ Statistics/PopulationVariance.py
@@ -1,7 +1,14 @@
 from Statistics.PopulationMean import population_mean
+from Calculators.Subtraction import subtraction
 from Calculators.Squaring import square
+from Calculators.Division import division
 
 
 def population_variance(data):
-    d = sum((square(data - population_mean)))/len(list)
+    u = population_mean(data)
+    deviations = subtraction(data, u)
+    sq_deviations = square(deviations)
+    x = len(data)
+    y = sum(sq_deviations)
+    d = division(x, y)
     return d
