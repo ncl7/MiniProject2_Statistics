ccbcc7f245a64919b74d523256b9aa4eaec7d1ca
ncl7 <ncl7@njit.edu>
2019-11-08 18:55:36 -0500

    Fix: corrected filepaths for Calculator CsvReader

--- Tests/test_Calculator.py
+++ Tests/test_Calculator.py
@@ -13,39 +13,39 @@ class MyTestCase(unittest.TestCase):
         self.assertIsInstance(self.calculator, Calculators)
 
     def test_subtraction(self):
-        test_data = CsvReader('./src/test_subtraction.csv').data
+        test_data = CsvReader("Tests/Data/test_subtraction.csv").data
         for row in test_data:
             self.assertEqual(self.calculator.subtract(int(row['Value 2']), int(row['Value 1'])), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_addition(self):
-        test_data = CsvReader('./src/test_addition.csv').data
+        test_data = CsvReader("Tests/Data/test_addition.csv").data
         for row in test_data:
             self.assertEqual(self.calculator.add(int(row['Value 1']), int(row['Value 2'])), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_multiply(self):
-        test_data = CsvReader('./src/test_multiplication.csv').data
+        test_data = CsvReader("Tests/Data/test_multiplication.csv").data
         pprint(test_data)
         for row in test_data:
             self.assertEqual(self.calculator.multiply(int(row['Value 1']), int(row['Value 2'])), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_division(self):
-        test_data = CsvReader('./src/test_division.csv').data
+        test_data = CsvReader("Tests/Data/test_division.csv").data
         for row in test_data:
             self.assertEqual(self.calculator.divide(int(row['Value 1']), int(row['Value 2'])), float(row['Result']))
             self.assertEqual(self.calculator.result, float(row['Result']))
 
     def test_square(self):
-        test_data = CsvReader('./src/test_square.csv').data
+        test_data = CsvReader("Tests/Data/test_square.csv").data
         pprint(test_data)
         for row in test_data:
             self.assertEqual(self.calculator.squaring(int(row['Value 1'])), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_square_root(self):
-        test_data = CsvReader('./src/test_square_root.csv').data
+        test_data = CsvReader("Tests/Data/test_square_root.csv").data
         pprint(test_data)
         for row in test_data:
             self.assertAlmostEqual(self.calculator.sqrt(int(row['Value 1'])), float(row['Result']), places=4)
