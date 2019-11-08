8d53068c4d720349143ba3b6a7e5b73370b758a4
Shannon <sp358@njit.edu>
2019-11-07 22:05:19 -0500

    Fix: updated code for pvalue static method

---
+++ Statistics/Pvalue.py
@@ -0,0 +1,11 @@
+from Statistics.ZScore import z_score
+
+
+def p_value(data):
+    data = []
+    n = len(data)
+    score = z_score()
+    for index in n:
+        if n == score:
+            data.append(index)
+    return index
