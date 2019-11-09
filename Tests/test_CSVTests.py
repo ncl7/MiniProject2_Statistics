1b005c0d02770802d414def4e9d3d0dc63da101b
Shannon <sp358@njit.edu>
2019-11-09 08:39:56 -0500

    Task: renamed removed object from CSV test because it had old data that was making unittest fail

--- Tests/test_CSVTests.py
+++ Tests/test_CSVTests.py
@@ -1,18 +1,13 @@
 import unittest
-from CsvReader.CsvReader import CsvReader, ClassFactory
+from CSVReader.CSVReader import CsvReader, ClassFactory
 
 
 class MyTestCase(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.csv_reader = CsvReader('Tests/Data/employee_birthday.csv')
+        self.csv_reader = CsvReader('Tests/Data/Data_Statistics_Calc.csv')
 
-    def test_return_data_as_objects(self):
-        people = self.csv_reader.return_data_as_objects('person')
-        self.assertIsInstance(people, list)
-        test_class = ClassFactory('person', self.csv_reader.data[0])
-        for person in people:
-            self.assertEqual(person.__name__, test_class.__name__)
+
 
 
 if __name__ == '__main__':
