269250e357bdd34032e779cba2cd14726d8d58d5
Shannon <sp358@njit.edu>
2019-11-09 08:37:07 -0500

    Fix: changed statistics import folder for sample standard deviation

--- Statistics/ZScore.py
+++ Statistics/ZScore.py
@@ -1,9 +1,9 @@
 from Statistics.PopulationMean import population_mean
-from Statistics.SampleStandardDeviation import sampleSTD
+from Statistics.SampleStandardDeviation import sample_st_dev
 from Calculators.Division import division
 
 
 def z_score(data):
     u = population_mean(data)
-    sam_std = sampleSTD(data)
+    sam_std = sample_st_dev(data)
     return division((len(data) - u), sam_std)
