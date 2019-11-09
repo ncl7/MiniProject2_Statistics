8872a9014f938ff6b7732f7ca55fd92448542735
Shannon <sp358@njit.edu>
2019-11-09 08:36:05 -0500

    Fix: Added additional stat methods to statistics file (proportion, pvalue, sample mean, sample std, sam var prop

--- Statistics/Statistics.py
+++ Statistics/Statistics.py
@@ -6,7 +6,11 @@ from Statistics.PopulationMean import population_mean
 from Statistics.Median import median
 from Statistics.Mode import mode
 from Statistics.VariancePopulationProportion import var_pop_proportion
-
+from Statistics.Proportion import proportion
+from Statistics.SampleVariance import var_sample_proportion
+from Statistics.SampleMean import sampleMean
+from Statistics.Pvalue import p_value
+from Statistics.SampleStandardDeviation import sample_st_dev
 from CSVReader.CSVReader import CsvReader
 
 
@@ -14,11 +18,11 @@ class Statistics(Calculator):
     data = []
 
     def __init__(self):
-        self.data = CsvReader(filepath)
+        self.data = CsvReader('Tests/Data/Data_Statistics_Calc.csv')
         super().__init__()
 
     def pop_mean(self):
-        self.result = population_mean(self.data)
+        self.result = population_mean(float(self.data))
         return self.result
 
     def med(self):
@@ -43,4 +47,24 @@ class Statistics(Calculator):
 
     def population_variance(self, d):
         self.result = population_variance(d)
+        return self.result
+
+    def proportion(self,):
+        self.result = proportion()
+        return self.result
+
+    def p_value(self,):
+        self.result = p_value()
+        return self.result
+
+    def sample_mean(self):
+        self.result = sampleMean()
+        return self.result
+
+    def sample_st_dev(self):
+        self.result = sample_st_dev()
+        return self.result
+
+    def var_sam_prop(self):
+        self.result = var_sample_proportion()
         return self.result
