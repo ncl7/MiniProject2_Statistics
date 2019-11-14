a61a14d6c0d8d27734de2896c3cb42bfa1ee6bad
Kelley Shann <kelleyshann@gmail.com>
2019-11-13 20:34:49 -0500

    task: fixed unit tests

--- Tests/test_Statistics.py
+++ Tests/test_Statistics.py
@@ -42,52 +42,52 @@ class MyTestCase(unittest.TestCase):
             #self.assertEqual(self.statistics.result, test_result(row['Population SD (Female)']))
 
     def test_var_pop_proportion(self):
-        test_data = CsvReader('Tests/Data/female_data.csv').data
+        test_data = CsvReaderStats('Tests/Data/female_height.csv').data
         test_result = CsvReader('Tests/Data/Results_Statistics_Calc.csv').data
         for row in test_result:
             self.assertEqual(self.statistics.variance_pop_proportion(test_data), float(row['Var Population Prop']))
            #self.assertEqual(self.statistics.result, test_result(row['Var Population Prop']))
 
     def test_proportion(self):
-        test_data = CsvReader('Tests/Data/female_data.csv').data
+        test_data = CsvReaderStats('Tests/Data/female_height.csv').data
         test_result = CsvReader('Tests/Data/Results_Statistics_Calc.csv').data
-        for row in test_data:
-            self.assertEqual(self.statistics.proportion(), float(row['Proportion']))
-            self.assertEqual(self.statistics.result, test_result(row['Proportion']))
+        for row in test_result:
+            self.assertEqual(self.statistics.proportion(test_data), float(row['Proportion']))
+            #self.assertEqual(self.statistics.result, test_result(row['Proportion']))
 
     def test_sample_mean(self):
         test_data = CsvReader('Tests/Data/female_data.csv').data
         test_result = CsvReader('Tests/Data/Results_Statistics_Calc.csv').data
         for row in test_data:
-            self.assertEqual(self.statistics.sample_mean(), float(row['Sample Mean']))
+            self.assertEqual(self.statistics.sample_mean(test_data), float(row['Sample Mean']))
             self.assertEqual(self.statistics.result, test_result(row['Sample Mean']))
 
     def test_sample_st_dev(self):
         test_data = CsvReader('Tests/Data/female_data.csv').data
         test_result = CsvReader('Tests/Data/Results_Statistics_Calc.csv').data
         for row in test_data:
-            self.assertEqual(self.statistics.sample_st_dev(), float(row['Sample SD']))
+            self.assertEqual(self.statistics.sample_st_dev(test_data), float(row['Sample SD']))
             self.assertEqual(self.statistics.result, test_result(row['Sample SD']))
 
     def test_sample_var_prop(self):
         test_data = CsvReader('Tests/Data/female_data.csv').data
         test_result = CsvReader('Tests/Data/Results_Statistics_Calc.csv').data
-        for row in test_data:
-            self.assertEqual(self.statistics.var_sam_prop(), float(row['Variance of Sample Proportion']))
+        for row in test_result:
+            self.assertEqual(self.statistics.var_sam_prop(test_data), float(row['Variance of Sample Proportion']))
             self.assertEqual(self.statistics.result, test_result(row['Variance of Sample Proportion']))
 
     def test_p_value(self):
         test_data = CsvReader('Tests/Data/female_data.csv').data
         test_result = CsvReader('Tests/Data/Results_Statistics_Calc.csv').data
-        for row in test_data:
-            self.assertEqual(self.statistics.p_value(), float(row['P Value']))
+        for row in test_result:
+            self.assertEqual(self.statistics.p_value(test_data), float(row['P Value']))
             self.assertEqual(self.statistics.result, test_result(row['P Value']))
 
     def test_z_score(self):
         test_data = CsvReader('Tests/Data/female_data.csv').data
         test_result = CsvReader('Tests/Data/Results_Statistics_Calc.csv').data
-        for row in test_data:
-            self.assertEqual(self.statistics.z_score(), float(row['ZScore']))
+        for row in test_result:
+            self.assertEqual(self.statistics.z_score(test_data), float(row['ZScore']))
             self.assertEqual(self.statistics.result, test_result(row['ZScore']))
 
 
