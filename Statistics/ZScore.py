e78dd85937eda128956f278d9e2a71121730fa74
ncl7 <ncl7@njit.edu>
2019-11-13 22:05:46 -0500

    Fix: adding missing "data" statement

--- Statistics/ZScore.py
+++ Statistics/ZScore.py
@@ -7,7 +7,7 @@ from Calculators.Division import division
 def z_score(data):
     data = [num for elem in data for num in elem]
     new_data = [float(x) for x in data]
-    x = new_data[1]
+    x = 64
     u = population_mean(new_data)
     sample_sd = sample_st_dev(new_data)
     y = subtraction(x, u)
