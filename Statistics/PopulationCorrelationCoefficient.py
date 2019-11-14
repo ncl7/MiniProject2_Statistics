4833a0bdcae489f6b43605853fb8f376d66e58bd
ncl7 <ncl7@njit.edu>
2019-11-13 19:50:41 -0500

    Fix: corrected code for population correlation coefficient

--- Statistics/PopulationCorrelationCoefficient.py
+++ Statistics/PopulationCorrelationCoefficient.py
@@ -7,16 +7,20 @@ from Calculators.Calculator import division
 
 
 def pop_correlation_coefficient(data):
-    x_data = CsvReader('Tests/Data/female_height.csv').data
-    y_data = CsvReader('Tests/Data/male_height.csv').data
-    x = pop_stand_dev(x_data)
-    y = pop_stand_dev(y_data)
+    # x_data = CsvReader('Tests/Data/female_height.csv').data
+    # y_data = CsvReader('Tests/Data/male_height.csv').data
+    x_data = [num for elem in data for num in elem]
+    y_data = [num for elem in data for num in elem]
+    new_x_data = [float(x) for x in x_data]
+    new_y_data = [float(x) for x in y_data]
+    x = pop_stand_dev(new_x_data)
+    y = pop_stand_dev(new_y_data)
     divisor = multiplication(x, y)
-    z = len(data)
+    z = len(new_x_data)
 
     # Covariance calculation:
-    a = subtraction(x_data, population_mean(x_data))
-    b = subtraction(y_data, population_mean(y_data))
+    a = subtraction(new_x_data, population_mean(new_x_data))
+    b = subtraction(new_y_data, population_mean(new_y_data))
     c = multiplication(a, b)
     covariance = division(z, (sum(c)))
 
