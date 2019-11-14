f0282c9a7a2169ff024839a4d11c33db402fc7d0
Kelley Shann <kelleyshann@gmail.com>
2019-11-13 19:15:46 -0500

    fix: corrected mode data task: added round function to mode

--- Statistics/Mode.py
+++ Statistics/Mode.py
@@ -1,4 +1,4 @@
 def mode(data):
     data = [num for elem in data for num in elem]
     new_data = [float(x) for x in data]
-    return max(set(new_data), key=new_data.count)
+    return round(max(set(new_data), key=new_data.count), 1)
