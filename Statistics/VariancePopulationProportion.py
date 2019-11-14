98a8c999c6979278ba93809b4813812c62ad7a2e
Kelley Shann <kelleyshann@gmail.com>
2019-11-13 20:35:39 -0500

    fix: corrected var pop prop formula

--- Statistics/VariancePopulationProportion.py
+++ Statistics/VariancePopulationProportion.py
@@ -6,8 +6,8 @@ from Calculators.Multiplication import multiplication
 
 def var_pop_proportion(data):
     p = proportion(data)
-    q = subtraction(p, 1)
+    q = subtraction(1, p)
     data = [num for elem in data for num in elem]
     new_data = [float(x) for x in data]
     n = len(new_data)
-    return division(multiplication(p, q), n)
+    return division(n, multiplication(p, q))
