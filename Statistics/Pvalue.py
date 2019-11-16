ece94568b077972150dc27a8dc4968df5c2695d7
ncl7 <ncl7@njit.edu>
2019-11-16 15:40:37 -0500

    Fix: changed data type to be iterable for n

--- Statistics/Pvalue.py
+++ Statistics/Pvalue.py
@@ -2,10 +2,10 @@ from Statistics.Z_Score import z_score
 
 
 def p_value(data):
-    data = []
+    # data = [data]
     n = len(data)
     score = z_score(data)
-    for index in n:
+    for index in range(n):
         if n == score:
             data.append(index)
     return index
