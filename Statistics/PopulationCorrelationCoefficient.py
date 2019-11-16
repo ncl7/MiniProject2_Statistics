6f4bf2ffa26f1b90556347c14e193b269ac43f57
ncl7 <ncl7@njit.edu>
2019-11-16 14:00:46 -0500

    Fix: correcting code for population correlation coefficient

--- Statistics/PopulationCorrelationCoefficient.py
+++ Statistics/PopulationCorrelationCoefficient.py
@@ -6,18 +6,18 @@ from Calculators.Calculator import division
 
 
 def pop_correlation_coefficient(data_x, data_y):
-    x_data = [num for elem in data_x for num in elem]
-    y_data = [num for elem in data_y for num in elem]
-    new_x_data = [float(x) for x in x_data]
-    new_y_data = [float(x) for x in y_data]
+    # x_data = [num for elem in data_x for num in elem]
+    # y_data = [num for elem in data_y for num in elem]
+    new_x_data = [float(x) for x in data_x]
+    new_y_data = [float(x) for x in data_y]
     x = pop_stand_dev(new_x_data)
     y = pop_stand_dev(new_y_data)
     divisor = multiplication(x, y)
     z = len(new_x_data)
 
     # Covariance calculation:
-    a = subtraction(x_data, population_mean(x_data))
-    b = subtraction(y_data, population_mean(y_data))
+    a = subtraction(data_x, population_mean(data_x))
+    b = subtraction(data_y, population_mean(data_y))
     c = multiplication(a, b)
     covariance = division(z, (sum(c)))
 
