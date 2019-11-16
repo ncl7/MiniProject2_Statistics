8fc88cb3209af00078321c82edd5e4f13cee9c22
ncl7 <ncl7@njit.edu>
2019-11-16 12:23:04 -0500

    Fix: removed unnecessary comment

--- Statistics/Z_Score.py
+++ Statistics/Z_Score.py
@@ -9,7 +9,6 @@ def z_score(data):
     # data = [float(num) for elem in range(data) for num in elem]
     new_data = [float(x) for x in data]
     x = new_data[1]
-    # X Value: 0.5641974007
     pop_sd = pop_stand_dev(new_data)
     y = subtraction(x, u)
     return division(pop_sd, y)
