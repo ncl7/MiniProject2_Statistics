f11ecd4bcf3c31163a00627e51bc0b24f069586c
ncl7 <ncl7@njit.edu>
2019-11-16 18:25:37 -0500

    Fix: corrected variables and removed unnecessary code for pop_correlation_coefficient

--- Statistics/PopulationCorrelationCoefficient.py
+++ Statistics/PopulationCorrelationCoefficient.py
@@ -1,27 +1,20 @@
 from Statistics.PopulationStandardDeviation import pop_stand_dev
 from Statistics.PopulationMean import population_mean
 from Calculators.Calculator import multiplication
-from Calculators.Calculator import subtraction
 from Calculators.Calculator import division
 
 
 def pop_correlation_coefficient(data_x, data_y):
-    # x_data = [num for elem in range(data_x) for num in elem]
-    # y_data = [num for elem in range(data_y) for num in elem]
-    # new_x_data = [range(x) for x in data_x]
-    # new_y_data = [range(x) for x in data_y]
-    new_x_data = data_x
-    new_y_data = data_y
-    x = pop_stand_dev(new_x_data)
-    y = pop_stand_dev(new_y_data)
+    x = pop_stand_dev(data_x)
+    y = pop_stand_dev(data_y)
     divisor = multiplication(x, y)
-    z = len(new_x_data)
+    z = len(data_x)
 
     # Covariance calculation:
-    d = population_mean(new_x_data)
-    e = population_mean(new_y_data)
-    a = [(element - d) for element in new_x_data]
-    b = [(element - e) for element in new_y_data]
+    d = population_mean(data_x)
+    e = population_mean(data_y)
+    a = [(element - d) for element in data_x]
+    b = [(element - e) for element in data_y]
     for index, item in enumerate(a):
         product = a[index] * b[index]
         index += 1
