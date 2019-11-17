3336691653cfcb5f3830f3e740279512fcbe37cf
ncl7 <ncl7@njit.edu>
2019-11-17 18:21:05 -0500

    Feature: created variable for list size

--- Statistics/PopulationCorrelationCoefficient.py
+++ Statistics/PopulationCorrelationCoefficient.py
@@ -8,18 +8,17 @@ def pop_correlation_coefficient(data_x, data_y):
     x = pop_stand_dev(data_x)
     y = pop_stand_dev(data_y)
     divisor = multiplication(x, y)
-    z = len(data_x)
 
     # Covariance calculation:
     d = population_mean(data_x)
     e = population_mean(data_y)
     a = [(element - d) for element in data_x]
     b = [(element - e) for element in data_y]
-
-    product = [a[i] * b[i] for i in range(len(a))]
+    size = len(a)
+    product = [a[i] * b[i] for i in range(size)]
     total = sum(product)
     print(total)
-    covariance = division(z, total)
+    covariance = division(size, total)
     print(covariance)
 
     # Population Correlation Coefficient calculation:
