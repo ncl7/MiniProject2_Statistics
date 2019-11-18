614cad3aba1a8069dc1e630f843f5656701260db
Shannon <sp358@njit.edu>
2019-11-17 23:41:31 -0500

    Fix: updated pvalue code to reflect change in lookup value

--- Statistics/Pvalue.py
+++ Statistics/Pvalue.py
@@ -4,6 +4,6 @@ from Statistics.Z_Score import z_score
 def p_value(data):
     score = z_score(data)
     value = [float(x) for x in data]
-    if value == score[0]:
+    if score == value[1]:
         return value
     print(value)
