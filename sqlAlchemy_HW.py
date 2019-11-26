77bda3ee544a1139d38c1f64b66d769a20893134
ncl7 <ncl7@njit.edu>
2019-11-25 22:04:32 -0500

    Feature: added customers table

--- sqlAlchemy_HW.py
+++ sqlAlchemy_HW.py
@@ -10,13 +10,15 @@ from datetime import datetime
 
 metadata = MetaData()
 
-blog = Table('blog', metadata,
-             Column('id', Integer(), primary_key=True),
-             Column('post_title', String(200), nullable=False),
-             Column('post_slug', String(200), nullable=False),
-             Column('content', Text(), nullable=False),
-             Column('published', Boolean(), default=False),
-             Column('created_on', DateTime(), default=datetime.now),
-             Column('updated_on', DateTime(), default=datetime.now, onupdate=datetime.now)
-             )
+customers = Table('customers', metadata,
+    Column('id', Integer(), primary_key=True),
+    Column('first_name', String(100), nullable=False),
+    Column('last_name', String(100), nullable=False),
+    Column('username', String(50), nullable=False),
+    Column('email', String(200), nullable=False),
+    Column('address', String(200), nullable=False),
+    Column('town', String(50), nullable=False),
+    Column('created_on', DateTime(), default=datetime.now),
+    Column('updated_on', DateTime(), default=datetime.now, onupdate=datetime.now)
+)
 
