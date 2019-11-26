414502239e251c9e7cf9901f50a9ad81cdc6c926
ncl7 <ncl7@njit.edu>
2019-11-25 21:40:56 -0500

    Feature: created engine for sqlite

--- sqlAlchemy_HW.py
+++ sqlAlchemy_HW.py
@@ -1,3 +1,9 @@
 import sqlalchemy
-sqlalchemy.__version__ 
+sqlalchemy.__version__
 '1.2.2'
+
+from sqlalchemy import create_engine
+engine = create_engine('sqlite:///sqlite3.db')
+# using relative path
+
+
