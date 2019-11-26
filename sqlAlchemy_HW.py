68b194a82596e2de37dcbbf9c4c150280519e2a2
ncl7 <ncl7@njit.edu>
2019-11-25 21:59:35 -0500

    Feature: adding meta-data to a table

--- sqlAlchemy_HW.py
+++ sqlAlchemy_HW.py
@@ -1,6 +1,22 @@
 from sqlalchemy import create_engine
-engine = create_engine('sqlite:///sqlite3.db')
+engine = create_engine('sqlite:///:memory:')
 # using relative path
 engine.connect()
 
 print(engine)
+
+from sqlalchemy import MetaData, Table, String, Column, Text, DateTime, Boolean, Integer
+from datetime import datetime
+
+metadata = MetaData()
+
+blog = Table('blog', metadata,
+             Column('id', Integer(), primary_key=True),
+             Column('post_title', String(200), nullable=False),
+             Column('post_slug', String(200), nullable=False),
+             Column('content', Text(), nullable=False),
+             Column('published', Boolean(), default=False),
+             Column('created_on', DateTime(), default=datetime.now),
+             Column('updated_on', DateTime(), default=datetime.now, onupdate=datetime.now)
+             )
+
