11adc9e167b79247505950583fb76beabc820853
ncl7 <ncl7@njit.edu>
2019-11-27 21:47:15 -0500

    Fix: changed filepath

--- Database/sqlalchemy_declarative.py
+++ Database/sqlalchemy_declarative.py
@@ -29,7 +29,7 @@ class Address(Base):
 
 
 # Create an engine that stores data in the local directory's sqlalchemy_example.db file.
-engine = create_engine('sqlite:///Sqlite-Data/sqlalchemy_example.db')
+engine = create_engine('sqlite:///sqlalchemy_example.db')
 
 # Create all tables in the engine. This is equivalent to "Create Table"
 # statements in raw SQL.
