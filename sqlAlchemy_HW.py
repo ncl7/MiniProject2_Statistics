8ca8d086a97e9a26fa8b38242bcb662e79ae0868
ncl7 <ncl7@njit.edu>
2019-11-27 19:24:00 -0500

    Feature: added print statements

--- sqlAlchemy_HW.py
+++ sqlAlchemy_HW.py
@@ -3,3 +3,7 @@ engine = db.create_engine('sqlite:///census.sqlite')
 connection = engine.connect()
 metadata = db.MetaData()
 census = db.Table('census', metadata, autoload=True, autoload_with=engine)
+# Print the column names
+print(census.columns.keys())
+# Print full table metadata
+print(repr(metadata.tables['census']))
