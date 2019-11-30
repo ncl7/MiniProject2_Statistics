0052d131759199c4ac10716cf83df3de296fa21e
ncl7 <ncl7@njit.edu>
2019-11-30 00:00:17 -0500

    Feature: added code to show items that start with "Wa" and have "e"

--- Database/sqlite_create.py
+++ Database/sqlite_create.py
@@ -363,3 +363,9 @@ session.query(
     cast("2010-12-01", Date),
 ).all()
 
+# Unions
+s1 = session.query(Item.id, Item.name).filter(Item.name.like("Wa%"))
+s2 = session.query(Item.id, Item.name).filter(Item.name.like("%e%"))
+s1.union(s2).all()
+s1.union_all(s2).all()
+
