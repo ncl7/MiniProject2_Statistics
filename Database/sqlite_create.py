e47afb678251a3f941b667b9ea7e13c4c58b9699
ncl7 <ncl7@njit.edu>
2019-11-29 23:53:01 -0500

    Feature: joined data for John Green from the Customer, Item, Order, and OrderLine tables

--- Database/sqlite_create.py
+++ Database/sqlite_create.py
@@ -301,3 +301,18 @@ session.query(Item).filter(Item.name.ilike("wa%")).order_by(Item.cost_price).all
 from sqlalchemy import desc
 session.query(Item).filter(Item.name.ilike("wa%")).order_by(desc(Item.cost_price)).all()
 
+# Querying Data with the join() method
+session.query(Customer).join(Order).all()
+print(session.query(Customer).join(Order))
+session.query(Customer.id, Customer.username, Order.id).join(Order).all()
+
+session.query(
+    Customer.first_name,
+    Item.name,
+    Item.selling_price,
+    OrderLine.quantity
+).join(Order).join(OrderLine).join(Item).filter(
+    Customer.first_name == 'John',
+    Customer.last_name == 'Green',
+    Order.id == 1,
+).all()
