38ebb8245833fd231b0b3e89212f35614df5c499
ncl7 <ncl7@njit.edu>
2019-11-29 22:31:43 -0500

    Feature: added more order lines to the session

--- Database/sqlite_create.py
+++ Database/sqlite_create.py
@@ -177,3 +177,20 @@ session.add_all([o1, o2])
 
 session.new
 session.commit()
+
+o3 = Order(customer=c1)
+
+orderline1 = OrderLine(item=i1, quantity=5)
+orderline2 = OrderLine(item=i2, quantity=10)
+
+o3.order_lines.append(orderline1)
+o3.order_lines.append(orderline2)
+
+session.add_all([o3])
+
+session.commit()
+
+c1.orders
+
+o1.customer
+
